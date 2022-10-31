2c634abe005ac226549106c359b0d23479124992
Christopher Ridder <christopher.ridder@stud.uni-due.de>
2022-10-31 13:05:38 +0100

    Entity Subject added

---
+++ jack3-product/jack3-core/src/main/java/de/uni_due/s3/jack3/entities/tenant/Subject.java
@@ -0,0 +1,35 @@
+package de.uni_due.s3.jack3.entities.tenant;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import de.uni_due.s3.jack3.entities.AbstractEntity;
+import org.hibernate.annotations.Type;
+import org.hibernate.envers.Audited;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import java.io.Serializable;
+
+/**
+ * Representation of a subject to which courses and exercises are assigned.
+ */
+
+@Audited
+@Entity
+@XStreamAlias("Subject")
+public class Subject extends AbstractEntity implements Serializable {
+
+	private static final long serialVersionUID = -4498420848759767490L;
+
+	@Column
+	@Type(type = "text")
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(final String name) {
+		this.name = requireIdentifier(name, "You must provide a non-empty name.");
+	}
+
+}
